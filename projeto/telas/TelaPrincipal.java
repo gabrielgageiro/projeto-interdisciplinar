368d3a0fab7eed040f3c727818e8900708c10130
gabrielgageiro <gabrielgageiro@gmail.com>
2017-10-24 17:42:55 -0200

    nome em janela

--- projeto/telas/TelaPrincipal.java
+++ projeto/telas/TelaPrincipal.java
@@ -25,6 +25,7 @@ public class TelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipal() {
+		super("Controle Principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
                 setVisible(true);
